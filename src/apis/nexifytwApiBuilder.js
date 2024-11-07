eba51e76b124a29403ce6c9b4a49836146de6234
盧偉弘 <luweihongmacminim1@luweihongdeMini.home>
2024-11-07 14:46:38 +0800

    feat:update github page

--- src/apis/nexifytwApiBuilder.js
+++ src/apis/nexifytwApiBuilder.js
@@ -1,10 +1,14 @@
 import AxiosBuilder from './base/axiosBuilder'
 import AxiosDirector from './base/axiosDirector'
 
+const ORIGIN_URL = 'http://nexifytw.mynetgear.com:45000/api/Record/'
+const CLOUDEFLARE_WORKER_URL = 'https://lively-river-3c62.z0809777.workers.dev/api/Record/'
+const currentUrl = import.meta.env.MODE === 'production' ? CLOUDEFLARE_WORKER_URL : ORIGIN_URL
+
 class NexifytwApiBuilder extends AxiosBuilder {
   constructor() {
     super()
-    this.setProperty('baseURL', 'http://nexifytw.mynetgear.com:45000/api/Record/')
+    this.setProperty('baseURL', currentUrl)
   }
 }
 
